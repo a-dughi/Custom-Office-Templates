715f9061c2f526eb9e8e5df8620fbf6c678e9828
Lillian Sequeira <lillian.g.sequeira@gmail.com>
2024-05-28 02:34:46 -0700

    Adding coin counter(fail)

--- Scenes/coin.gd
+++ Scenes/coin.gd
@@ -1,9 +1,10 @@
 extends Area2D
 
-
+@onready var coin_text = $CanvasLayer/CoinCounter		
 var coinCount = 0
 func _on_body_entered(body):
 	queue_free()
 	coinCount += 1
+	#coin_text.text = "Coins: " + str(coinCount)
 	
 	
--- Scripts/game.gd
+++ Scripts/game.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 var block = preload("res://Scenes/animatable_body_2d.tscn")
 var coin = preload("res://Scenes/coin.tscn")
+var body = preload("res://Scenes/character_body_2d.tscn")
 
 var xPos = []
 var yPos = []
+var coinArray = []
 var numLevels = 6
 
 # Called when the node enters the scene tree for the first time.
@@ -24,6 +26,7 @@ func _ready():
 			var new_coin
 			if(i%3==0):
 				new_coin = coin.instantiate()
+				coinArray.insert(i/3, new_coin)
 				new_coin.global_position.x = xPos[i]
 				new_coin.global_position.y = yPos[i]-10
 			
@@ -35,7 +38,9 @@ func _position():
 	for i in range(numLevels):	
 		var j = 0;
 		while j<16:
+			#constant range of x values
 			var xTemp = randf_range(-50, 100)
+			#changes range of y values based on level
 			var yTemp = randf_range(-400*(i+1), (-400*(i+1))+400)
 			var present = false
 			
@@ -56,12 +61,19 @@ func _position():
 			
 
 
-				
+@onready var coin_text = $CanvasLayer/CoinCounter				
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	
 	pass
+	
+	#var numCoins = 0
+	#for i in range(coinArray.size()):
+		#if coinArray[i]._on_body_entered(body):
+			#numCoins = numCoins +1
+			#
+	#coin_text.text = str(numCoins)	
+	
 	
 	
 	
