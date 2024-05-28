b21b5f2fcc98b4bd759d2992c2a7317df022c32b
Lillian Sequeira <lillian.g.sequeira@gmail.com>
2024-05-27 19:59:16 -0700

    Fixed blocks issue, added position function

--- Scripts/game.gd
+++ Scripts/game.gd
@@ -1,32 +1,25 @@
 extends Node2D
 
 var block = preload("res://Scenes/animatable_body_2d.tscn")
-#var movingBlock = preload("res://Scenes/movingBlock.tscn")
+
 var xPos = []
 var yPos = []
 var numLevels = 1
-#figure out how to bypass the first thing, figure out array access issue
+
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	
-	
 	# Replace with function body.
-		
+		_position()
+	
 		for i in range(10):
 			var new_block = block.instantiate()
 			
-			new_block.global_position.x = xPos[i] #randf_range(-100, 100) 
-			new_block.global_position.y = yPos[i] #randf_range(-100, 100)
+			new_block.global_position.x = xPos[i]  
+			new_block.global_position.y = yPos[i] 
 			
 			self.add_child(new_block)
 			
-		#for i in range(10):
-			#var newBlock = movingBlock.instantiate()
-			#newBlock.global_position.x = randf_range(0,1152)
-			#newBlock.global_position.y = randf_range(200, 300)
-			
-			#self.add_child(newBlock)
 			
 func _position():
 	
@@ -52,13 +45,6 @@ func _position():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	
-	#for i in range(20):
-		#var new_block = block.instantiate()
-			#
-		#new_block.global_position.x = randf_range(0, 200) 
-		#new_block.global_position.y = randf_range(-100, 100)
-			#
-		#self.add_child(new_block)
 	pass
 	
 	
